0f53d02a98628392f14c07fc8b1c1b52e777df7d
AditiSinghal11 <aditi247ps@gmail.com>
2025-08-10 23:55:30 +0530

    navigation bar but idk how to run the code :)

--- frontend/src/components/navabr.tsx
+++ frontend/src/components/navabr.tsx
@@ -1,24 +1,37 @@
 import {
   NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
+  NavigationMenuList,
   NavigationMenuItem,
   NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
-} from "@/components/ui/navigation-menu"
-export default function Navbar() {
-    return(<NavigationMenu>
-  <NavigationMenuList>
-    <NavigationMenuItem>
-      <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-        <NavigationMenuTrigger>Item two</NavigationMenuTrigger>
-      <NavigationMenuContent>
-        <NavigationMenuLink>Link</NavigationMenuLink>
-      </NavigationMenuContent>
-    </NavigationMenuItem>
-  </NavigationMenuList>
-</NavigationMenu>)
-}
+} from "@/components/ui/navigation-menu";
+import Link from "next/link";
 
+export default function Navbar() {
+  return (
+    <NavigationMenu className="bg-black text-white px-6 py-3 shadow-md w-full justify-end">
+      <NavigationMenuList className="flex space-x-6">
+        <NavigationMenuItem>
+          <Link href="/profile" passHref legacyBehavior>
+            <NavigationMenuLink className="hover:text-orange-500 transition-colors">
+              Profile
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/my-parties" passHref legacyBehavior>
+            <NavigationMenuLink className="hover:text-orange-500 transition-colors">
+              My Parties
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/settings" passHref legacyBehavior>
+            <NavigationMenuLink className="hover:text-orange-500 transition-colors">
+              Settings
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
