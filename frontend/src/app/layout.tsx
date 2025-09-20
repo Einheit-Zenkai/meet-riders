01971abeb76f2894fe529800316667264fe30af1
nithitsuki <nithilanr@gmail.com>
2025-09-20 15:45:35 +0530

    ui tweaks, centralised auth, refactoring

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -6,7 +6,6 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { ModeToggle } from "@/components/ModeToggle";
 import { Toaster } from "@/components/ui/sonner";
 import { AuthProvider } from "@/context/Authcontext";
-import Sidebar from "@/components/sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,26 +14,21 @@ export const metadata: Metadata = {
   description: "Meet and ride together",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({children}: any): any {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className}`}>
         <AuthProvider> {/* 👈 wrap entire app with AuthProvider */}
           <PartyProvider>
             <ThemeProvider>
-              {/* Global floating controls */}
               <div className="fixed top-4 right-4 z-50">
                 <ModeToggle />
               </div>
               <div className="min-h-screen pl-16">
                 <main>{children}</main>
-              </div>
               <Toaster />
-          </ThemeProvider>
+              </div>
+            </ThemeProvider>
           </PartyProvider>
         </AuthProvider>
       </body>
